f06e972a667580fbd27261d75fac87e303463ecf
Kellyn Arnold <K.F.Arnold@leeds.ac.uk>
2021-05-26 17:04:40 +0100

    Update function with parameter specifiying whether message should be printed

--- Script/Functions/Import_Unloaded_CSV_Files.R
+++ Script/Functions/Import_Unloaded_CSV_Files.R
@@ -5,13 +5,16 @@
 #' be located anywhere within the project directory.
 #'
 #' @param filenames List of filenames (without ".csv")
+#' @param silent Whether to return message describing files loaded into
+#' global environment (T/F, default is FALSE)
 #'
-#' @return Message describing which of \code{filenames} were loaded into the
-#' global environment
+#' @return If \code{verbose = TRUE}, prints message describing which of 
+#' \code{filenames} were loaded into the global environment
 #'
 #' @examples
-#' Import_Unloaded_CSV_Files(filenames = list("knots_best", "summary_eur"))
-Import_Unloaded_CSV_Files <- function(filenames) {
+#' Import_Unloaded_CSV_Files(filenames = list("knots_best", "summary_eur"),
+#' silent = FALSE)
+Import_Unloaded_CSV_Files <- function(filenames, silent = FALSE) {
   
   # Determine which files are not already loaded in global environment
   filenames_missing <- setdiff(filenames, ls(.GlobalEnv)) 
@@ -26,12 +29,14 @@ Import_Unloaded_CSV_Files <- function(filenames) {
     set_names(filenames_missing)
   list2env(dataframes, .GlobalEnv)
   
-  # Print names of files imported into global environment
-  if (length(filenames_missing) == 0) {
-    cat("All files already exist in the global environment.")
-  } else {
-    cat("Files imported to global environment:", 
-        paste(filenames_missing, collapse = ", "), sep = "\n")
+  # If specified, print names of files imported into global environment
+  if (silent == FALSE) {
+    if (length(filenames_missing) == 0) {
+      cat("All files already exist in the global environment.")
+    } else {
+      cat("Files imported to global environment:", 
+          paste(filenames_missing, collapse = ", "), sep = "\n")
+    }
   }
   
 }
