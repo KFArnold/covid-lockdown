d7aff23364ee2be6ef5213ee5485f31e7c700a46
Kellyn Arnold <K.F.Arnold@leeds.ac.uk>
2021-05-27 14:11:45 +0100

    Update aesthetics

--- Script/figure_aesthetics.R
+++ Script/figure_aesthetics.R
@@ -1,6 +1,18 @@
-library(RColorBrewer); library(scales)
+library(RColorBrewer); library(scales); library(ggpubr)
 
-# Simulation aesthetics --------------------------------------------------------
+# Import all files
+
+# Import required files
+Import_Unloaded_CSV_Files(filenames = c("Cases_deaths_data_europe",
+                                        "knots_best",
+                                        "thresholds_eur"), 
+                          silent = TRUE)
+Import_All_Simulated_Data(location = out_folder,
+                          silent = TRUE)
+
+# DEFINE AESTHETICS ------------------------------------------------------------
+
+## Simulation aesthetics -------------------------------------------------------
 
 # Simulation levels (defines ordering)
 simulation_levels <- c("0,0", "0,1", "0,3", "0,5", "0,7", 
@@ -34,7 +46,28 @@ simulation_aes <- simulation_aes %>%
   mutate(Label = Simulation_Labeller(Simulation)) %>%
   relocate(Label, .after = Simulation)
 
+## History aesthetics ----------------------------------------------------------
 
+# History levels (defines ordering)
+history_levels <- c("Natural history", "Counterfactual history")
+
+## Threshold aesthetics --------------------------------------------------------
+
+# Threshold levels (defines ordering)
+threshold_levels <- c("1 case per 100,000", "1 case per 20,000", "1 case per 10,000")
+
+# Create function which labels threshold levels
+# (e.g. if level is "1 case per 100,000", label is "1 case per\n100,000")
+Threshold_Labeller <- function(threshold_level) {
+  threshold_level %>%
+    gsub(pattern = "per ", replacement = "per\n")
+}
+
+# Create label, shape, and transparency key for threshold levels
+threshold_aes <- tibble(Threshold = threshold_levels,
+                        Label = Threshold_Labeller(Threshold),
+                        Shape = c(15, 16, 17),
+                        Alpha = c(0.4, 0.7, 1))
 
 
 
