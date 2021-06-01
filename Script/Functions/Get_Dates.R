6fb522eaeecdbea4f5484ba63dd602a8e81dfc4a
Kellyn Arnold <K.F.Arnold@leeds.ac.uk>
2021-06-01 10:35:50 +0100

    Update function to call First_Character_To_Lower function

--- Script/Functions/Get_Dates.R
+++ Script/Functions/Get_Dates.R
@@ -7,7 +7,9 @@
 #' @param dates Character vector of date variable(s) to get; 
 #' default is NULL, which returns all variables containing "Date"
 #'
-#' @return Named list containing values of all \code{dates} for \code{country}
+#' @return Named list containing values of all \code{dates} for \code{country};
+#' note that the first letter of each of \code{dates} is returned as a lowercase,
+#' to indicate that it represents the specific value of the input variable.
 #'
 #' @examples
 #' Get_Dates(country = "United Kingdom", dates = c("Date_start", "Date_T"))
@@ -34,7 +36,8 @@ Get_Dates <- function(country, dates = NULL) {
   }
   
   # Create list containing dates
-  dates_list <- dates_df %>% as.list
+  dates_list <- dates_df %>% as.list %>% 
+    setNames(., First_Character_To_Lower(names(.)))
   
   # Return list containing dates
   return(dates_list)
